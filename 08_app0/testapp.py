993ab6f032734e1e049f6fe73052738f792c5198
Jeff <jlin02@stuy.edu>
2019-09-18 14:48:37 -0400

    Added 08_app0 with Flask project.

---
+++ 08_app0/testapp.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    print("Main page loaded") # Prints out something on load/reload in console
+    return "This is the main page!"
+
+@app.route("/goodbye")
+def goodbye_world():
+    print("Goodbye page loaded")
+    return "This is a different page..."
+
+@app.route("/spooky")
+def spook_world():
+    print("Spooky page loaded")
+    return "BOO!"
+
+if __name__ == "__main__":
+    app.debug = True # Automatically updates project with save file
+    app.run()
