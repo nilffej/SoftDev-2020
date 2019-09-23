0bf63f1f41611d016c8c53b79286a3f19cda58e7
Jeff <jlin02@stuy.edu>
2019-09-23 14:51:06 -0400

    Edited backend of dictionary reading and percentage calculations.

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -4,26 +4,20 @@ import random
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
-# Reading in occupations file
-file = open("occupations.csv","r")
-readfile = file.readlines()
-readfile.pop(0)
 dictionary = {}
-percentages = []
 def sol():
-    total = 0;
-    for line in readfile:
-        idx = line.rfind(",")
-        job = line[0:idx]
-        percent = float(line[idx+1:-1])
-        percentages.append(percent)
-        dictionary[job] = round(percent + total,1);
-        total += percent
-    del dictionary["Total"]
+    with open("occupations.csv","r") as oldcsv:
+        csvRead = csv.reader(oldcsv)
+        next(csvRead)
+        for row in csvRead:
+            dictionary[row[0]] = float(row[1])
     rand = random.randint(1,998) * 0.1
+    tempsum = 0
+    print(dictionary)
+    print(dictionary.values())
     for key in dictionary.keys():
-        if rand <= dictionary[key]:
+        tempsum += dictionary[key]
+        if rand <= tempsum:
             return key
 
 @app.route("/")
@@ -34,11 +28,13 @@ def hello_world():
 @app.route("/occupyflaskst")
 def newpage():
     name = sol()
+    keys = list(dictionary.keys())
     return render_template('site.html',
     titulo = "Team Gold Introduction",
     job = name,
-    col1 = dictionary.keys(),
-    col2 = percentages
+    col1 = keys,
+    col2 = list(dictionary.values()),
+    lengthnum = range(len(keys))
     )
 
 if __name__ == "__main__":
