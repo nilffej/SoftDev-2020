6caa22886c6877ecdb941a18b94b1706ac836fde
Jeff Lin <jeff.andrew.lin@gmail.com>
2020-04-21 22:11:12 -0400

    oh yeah

---
+++ 18_d3/app.py
@@ -0,0 +1,16 @@
+# Jeff Lin + Jackson Zou
+# SoftDev PD 9
+# K18 Come Up For Air
+# 2020-04-21
+
+from flask import Flask, render_template, request, redirect, url_for
+from parse import *
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
--- 18_d3/parse.py
+++ 18_d3/parse.py
@@ -1,9 +1,20 @@
 from pprint import pprint
+from operator import itemgetter
+
+def output(data, head):
+    output = [head]
+    file = open('output.csv','w')
+    for item in data:
+        output.append(','.join(item) + '\n')
+    file.writelines(output)
 
 f = open('nycdeaths.csv','r')
 f = f.readlines()
 data = []
-for line in f:
+head = f[0]
+for line in f[1:]:
     data.append(line[:-1].split(','))
+data = sorted(data, key = itemgetter(0))
+data = sorted(data, key = itemgetter(1))
 
-pprint(data)
+output(data, head)
